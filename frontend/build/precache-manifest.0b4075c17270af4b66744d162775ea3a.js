51d1384acdd7fe7d86e8d7c5956905abd236f7b0
mrpandey1 <pandeynishant12345@gmail.com>
2020-07-30 18:40:55 +0530

    ..

---
+++ frontend/build/precache-manifest.0b4075c17270af4b66744d162775ea3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1057fe84b9573f789720a62af692ef1e",
+    "url": "/face-detection-web/index.html"
+  },
+  {
+    "revision": "bdd3b482d109b1814eda",
+    "url": "/face-detection-web/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e8999e5ecbd21203c168",
+    "url": "/face-detection-web/static/css/main.28d17ad6.chunk.css"
+  },
+  {
+    "revision": "bdd3b482d109b1814eda",
+    "url": "/face-detection-web/static/js/2.3b995de7.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-detection-web/static/js/2.3b995de7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8999e5ecbd21203c168",
+    "url": "/face-detection-web/static/js/main.45ca3720.chunk.js"
+  },
+  {
+    "revision": "7a4cd6276c6b626c544b",
+    "url": "/face-detection-web/static/js/runtime-main.472b374f.js"
+  }
+]);
