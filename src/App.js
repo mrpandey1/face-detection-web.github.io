05191556fa5af156099ef352a69fbaf0e3dff128
mrpandey1 <pandeynishant12345@gmail.com>
2020-10-14 13:34:12 +0530

    added rank section

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
       <Logo/>
+      <Rank/>
       <ImageLinkForm/>
       {/* <FaceRecognition/> */}
     </div>
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+const Rank =()=> {
+    return(
+        <div>
+            <div className='white f3'>
+            Nishant your Rank is 
+            </div>
+            <div className='white f1'>
+               #5
+            </div>
+        </div>
+    )
+}
+export default Rank;
