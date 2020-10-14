bb53d3df10a7307205a2e3bfa9e17f3afa436e5a
mrpandey1 <pandeynishant12345@gmail.com>
2020-10-14 13:45:32 +0530

    changed wallpaper with particle js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,24 @@ import Navigation from './components/Navigation/Navigation';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
-
+import Particles from 'react-particles-js';
+const particleOptions ={
+  particles:{
+    number:{
+      value:100,
+      density:{
+        enable:true,
+        value_area:800
+      }
+    }
+  }
+}
 function App() {
   return (
     <div className="App">
+      <Particles className='particles'
+        params={particleOptions}
+      />
       <Navigation/>
       <Logo/>
       <Rank/>
