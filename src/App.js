7d8188ba32c6a5250bacce952fe9dcbea3718e1e
mrpandey1 <pandeynishant12345@gmail.com>
2020-10-14 13:27:57 +0530

    added ImageLinkForm

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Logo from './components/Logo/Logo';
 
 function App() {
@@ -8,8 +9,8 @@ function App() {
     <div className="App">
       <Navigation/>
       <Logo/>
-      {/* <ImageLinkForm/>
-      <FaceRecognition/> */}
+      <ImageLinkForm/>
+      {/* <FaceRecognition/> */}
     </div>
   );
 }
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Tilt from 'react-tilt';
 import './Logo.css'
+import brain from './brain.png';
 const Logo = () =>{
     return (
         <div className='ma4 mt0'>
-            <Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
-                <div className="Tilt-inner"> 👽 </div>
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+                <div className="Tilt-inner pa3"> 
+                    <img src={brain} style={{paddingTop:'5px'}} alt='logo'/>
+                </div>
             </Tilt>
         </div>
     );
