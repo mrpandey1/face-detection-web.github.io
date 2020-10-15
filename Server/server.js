10feb3998f957f0f6ded3f50554b8753814c2946
mrpandey1 <pandeynishant12345@gmail.com>
2020-10-15 13:27:26 +0530

    express middle ware

--- Server/server.js
+++ Server/server.js
@@ -1,23 +1,16 @@
-const { request } = require("express");
-
 const express=require('express');
 
 const app= express();
+
+app.use((req,res,next)=>{
+    console.log('<h1>Hello</h1>');
+    next();
+})
+
 
 app.get('/',(req,res)=>{
     res.send('getting root');
 })
 
-app.get('/profile',(req,res)=>{
-    res.send('getting profile');
-})
-
-app.post('/profile ',(req,res)=>{
-    const user={
-        name:'nishant',
-        hobby:'soccer'
-    }
-    res.send(user);
-})
 
 app.listen(3000);
