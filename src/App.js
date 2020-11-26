ff654c6e2a45a36b256e7d534e1b629f591475fc
mrpandey1 <pandeynishant12345@gmail.com>
2020-11-26 11:50:44 +0530

    user profile update done

--- src/App.js
+++ src/App.js
@@ -78,7 +78,22 @@ class App extends Component {
     app.models.predict(
       Clarifai.FACE_DETECT_MODEL, 
       this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .then(response => {
+        if(response){
+          fetch('http://localhost:3000/image',{
+            method:'put',
+            headers:{'Content-Type':'application/json'},
+            body:JSON.stringify({
+              id:this.state.user.id
+            })
+          })
+          .then(response=>response.json())
+          .then(count=>{
+            this.setState(Object.assign(this.state.user,{entries:count}))
+          })
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
       .catch(err=>console.log(err));
   }
 
@@ -100,14 +115,17 @@ class App extends Component {
         <Navigation onRouteChange={this.onRouteChange} isSignedIn={isSignedIn}/>
         {route==='home'?<div>
               <Logo/>
-              <Rank/>
+              <Rank
+                name={this.state.user.name}
+                entries={this.state.user.entries}
+              />
               <ImageLinkForm 
               onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}
               />
               <FaceRecognition box={box} imageUrl={imageUrl}/>
             </div>:(
               route==='signin'?
-              <Signin onRouteChange={this.onRouteChange}/>:
+              <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>:
               <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
             )
             }
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,17 @@
-import { render } from '@testing-library/react';
+
 import React from 'react';
 
-const Rank = () => {
-    return(
-        <div>
-            <div className='white f3'>
-            Nishant your Rank is 
-            </div>
-            <div className='white f1'>
-               #5
-            </div>
-        </div>
-    )
+const Rank = ({ name, entries }) => {
+  return (
+    <div>
+      <div className='white f3'>
+        {`${name}, your current entry count is...`}
+      </div>
+      <div className='white f1'>
+        {entries}
+      </div>
+    </div>
+  );
 }
+
 export default Rank;
