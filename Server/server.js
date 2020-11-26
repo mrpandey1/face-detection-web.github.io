38701ac4478a5c27f50ef77dff89d6d4e28d562c
mrpandey1 <pandeynishant12345@gmail.com>
2020-11-26 16:52:44 +0530

    sign in done

--- Server/server.js
+++ Server/server.js
@@ -52,29 +52,52 @@ app.get('/',(req,res)=>{
 })
 
 app.post('/signin',(req,res)=>{
-    if(req.body.email===database.users[0].email && 
-        req.body.password=== database.users[0].password){
-            res.json(database.users[0]);
+    db.select('email','hash').from('login')
+    .where('email','=',req.body.email)
+    .then(data=>{
+        const isValid=bcrypt.compareSync(req.body.password,data[0].hash);
+        if(isValid){
+            return db.select('*').from('users')
+            .where('email','=',req.body.email)
+            .then(user=>{
+                res.json(user[0])
+            })
+            .catch(Err=>{
+                res.status(400).json('unable to get user');
+            })
+        }else{
+            res.status(400).json('wrong credentials');
         }
-        else{
-            res.status(400).json('error logging in');
-        }
+    })
+    .catch(err=>{
+        res.status(400).json("something went wrong");
+    })
 })
 
 app.post('/register',(req,res)=>{
     const { email, name, password}=req.body;
-    bcrypt.hash(password,null,null,function(err,hash){
-        // console.log(hash);
-    });
-    db('users')
-    .returning('*')
-    .insert({
-        email:email,
-        name:name,
-        joined:new Date()
-    })
-    .then(user=>{
-        res.json(user[0])
+    const hash=bcrypt.hashSync(password);
+    db.transaction(trx=>{
+        trx.insert({
+            hash:hash,
+            email:email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail=>{
+           return trx('users')
+            .returning('*')
+            .insert({
+                email:loginEmail[0],
+                name:name,
+                joined:new Date()
+            })
+            .then(user=>{
+                res.json(user[0])
+            })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
     })
     .catch(err=>res.status(400).json(err))
 })
