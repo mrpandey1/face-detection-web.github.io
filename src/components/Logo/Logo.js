fce1f61ebd6665b6ca0d73c1dd10093c54e0276d
mrpandey1 <pandeynishant12345@gmail.com>
2021-01-03 10:46:07 +0530

    changed the position on logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import './Logo.css'
 import brain from './brain.png';
 const Logo = () =>{
     return (
-        <div className='ma4 mt0'>
+        <div className='ma4 mt0 center'>
             <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
                 <div className="Tilt-inner pa3"> 
                     <img src={brain} style={{paddingTop:'5px'}} alt='logo'/>
